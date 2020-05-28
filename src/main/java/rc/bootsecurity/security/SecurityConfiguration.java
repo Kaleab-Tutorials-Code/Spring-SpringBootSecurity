40ed3adfc32c93281c245d660f1699104fcae370
kaleab <tadewoskaleab@gmail.com>
2020-05-27 23:20:27 -0500

    FORM BASED AUTHENTICATION - Configure remember me for you login page. Steps followed :  - add remember me checkbox inside your login.html.  - Note that spring is aware of a check box with name and id "remember-me" to use it.  - then tell spring about it  Code :            .rememberMe() : this by itself works without the others , it tells spring to use remember me.            .tokenValiditySeconds(2592000) : additional configuration to tell spring "this remember me will only valid for 2592000 seconds which is 1 month"            .key("mySecret!"); : if you don't like spring default hash secret , then you can add your own.

--- src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
+++ src/main/java/rc/bootsecurity/security/SecurityConfiguration.java
@@ -42,7 +42,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login").permitAll()
                 .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login");
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .and()
+                .rememberMe().tokenValiditySeconds(2592000).key("mySecret!");
     }
 
     //this is authentication provider configuration. everything UserpricipalDetailService is done for it.
