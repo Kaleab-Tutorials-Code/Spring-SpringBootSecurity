47a794a598a05b1a557c5359a3eebd268421d066
kaleab <tadewoskaleab@gmail.com>
2020-05-27 22:04:21 -0500

    Db Init :  - reads comments inside Security Configuration : - for configure(AuthenticationManagerBuilder auth) method         -- previosly user and their roles specified manually but now it can found from authenticationProvider which also setup here. - read comment for how authentication Provider setup.
    About : AuthenticationProviderService
    //this is authentication provider configuration. everything UserpricipalDetailService is done for it.
        //Configurations :
         // -- initialize DaoAuthenticationProvider
         // -- assign a userPricinpalDetailService to the authentication Provider
         // -- and return the authentication Provider.

--- src/main/java/rc/bootsecurity/db/DbInit.java
+++ src/main/java/rc/bootsecurity/db/DbInit.java
@@ -11,6 +11,8 @@ import java.util.List;
 @Service
 public class DbInit implements CommandLineRunner {
     private UserRepository userRepository;
+    //This encoder is added because our DAOAuthenticationProvider inside SecurityConfiguration Class
+    //needs a password to be encoded using passwordEncoder which is BCrypt. 
     private PasswordEncoder passwordEncoder;
 
     public DbInit(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -21,9 +23,11 @@ public class DbInit implements CommandLineRunner {
     @Override
     public void run(String... args) {
         // Delete all
+    	//added for fresh user data start.
         this.userRepository.deleteAll();
 
         // Crete users
+        //so as you see password is saved using password encoder.
         User dan = new User("dan",passwordEncoder.encode("dan123"),"USER","");
         User admin = new User("admin",passwordEncoder.encode("admin123"),"ADMIN","ACCESS_TEST1,ACCESS_TEST2");
         User manager = new User("manager",passwordEncoder.encode("manager123"),"MANAGER","ACCESS_TEST1");
